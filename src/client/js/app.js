b42a19db103dacc205e85502f884e0a16c2625d5
Debora Durek <debora.durek@gmail.com>
2021-03-03 21:12:10 -0600

    feat: Select user data input

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,2 +1,11 @@
 // Event listener to form
 document.querySelector('form').addEventListener('submit', handleSubmit);
+
+// Main function
+function handleSubmit(event) {
+  event.preventDefault();
+  // Get entered city and country code
+  const city = document.getElementById('city').value;
+  const countryCode = document.getElementById('country-code').value;
+  // Send input values to server endpoint
+}
