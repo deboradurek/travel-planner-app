748adfa7a16a7aae32a51f4f062553f946233a65
Debora Durek <debora.durek@gmail.com>
2021-03-03 21:14:11 -0600

    feat: Add function to post user input to /add

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -8,4 +8,25 @@ function handleSubmit(event) {
   const city = document.getElementById('city').value;
   const countryCode = document.getElementById('country-code').value;
   // Send input values to server endpoint
+  provideGeoNames({ city, countryCode });
 }
+
+// Function to POST input values, pointing to local server
+const provideGeoNames = async (data = {}) => {
+  const response = await fetch('http://localhost:3000/add', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const res = await response.json();
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
