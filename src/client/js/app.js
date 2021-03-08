79bed57e79712eaefb29eaef80e07f3166b8d655
Debora Durek <debora.durek@gmail.com>
2021-03-08 13:34:29 -0600

    feat: Update countdown and try/catch if error

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -9,16 +9,16 @@ function handleSubmit(event) {
   const countryCode = document.getElementById('country-code').value;
   const travelDate = document.getElementById('date').value;
   // Create countdown
-  const presentDate = new Date().getTime();
-  const futureDate = new Date(travelDate).getTime();
+  const presentDate = new Date().setHours(0);
+  const futureDate = new Date(travelDate).setHours(23);
   const countdown = Math.ceil((futureDate - presentDate) / (1000 * 60 * 60 * 24));
   console.log(countdown);
   // Send input values to server endpoint
-  provideGeoNames({ city, countryCode, countdown });
+  provideUserInput({ city, countryCode, travelDate, countdown });
 }
 
-// Function to POST input values, pointing to local server
-const provideGeoNames = async (data = {}) => {
+// Function to POST user input values, pointing to local server
+const provideUserInput = async (data = {}) => {
   const response = await fetch('http://localhost:3000/add', {
     method: 'POST',
     credentials: 'same-origin',
@@ -30,6 +30,11 @@ const provideGeoNames = async (data = {}) => {
 
   try {
     const res = await response.json();
+    // Validate if response is not successful
+    if (response.status !== 200) {
+      alert(res.message);
+      return null;
+    }
     return res;
   } catch (error) {
     console.log('error', error);
