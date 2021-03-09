8d27628e47fd7a3517dc4d201dc554bba830a64d
Debora Durek <debora.durek@gmail.com>
2021-03-09 13:31:08 -0600

    feat: Add function to update UI

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -12,9 +12,11 @@ function handleSubmit(event) {
   const presentDate = new Date().setHours(0);
   const futureDate = new Date(travelDate).setHours(23);
   const countdown = Math.ceil((futureDate - presentDate) / (1000 * 60 * 60 * 24));
-  console.log(countdown);
   // Send input values to server endpoint
-  provideUserInput({ city, countryCode, travelDate, countdown });
+  provideUserInput({ city, countryCode, travelDate, countdown }).then((allData) => {
+    // console.log(allData);
+    updateUI(allData);
+  });
 }
 
 // Function to POST user input values, pointing to local server
@@ -31,14 +33,39 @@ const provideUserInput = async (data = {}) => {
   try {
     const res = await response.json();
     // Validate if response is not successful
-    if (response.status !== 200) {
-      alert(res.message);
-      return null;
-    }
+    // if (response.status !== 200) {
+    //   alert(res.message);
+    //   return null;
+    // }
+    // console.log(res);
     return res;
   } catch (error) {
     console.log('error', error);
   }
 };
+
+// Function to Update UI
+function updateUI(allData) {
+  document.getElementById('city-name').innerHTML = `<p>${allData.city}</p>`;
+  // document.getElementById('state').innerHTML = `<p>${allData.state}</p>`;
+  document.getElementById('country').innerHTML = `<p>${allData.state} - ${allData.country}</p>`;
+  document.getElementById('travel-date').innerHTML = `<p>Travel Date: ${allData.travelDate}</p>`;
+  document.getElementById('countdown').innerHTML = `<p>${allData.countdown} day(s) to go!</p>`;
+  document.getElementById('temperature').innerHTML = `<p>${Math.round(
+    allData.dataCurrentWeather.temperature
+  )} °C</p>`;
+  document.getElementById('humidity').innerHTML = `<p>${allData.dataCurrentWeather.humidity} %</p>`;
+  document.getElementById('clouds').innerHTML = `<p>${allData.dataCurrentWeather.clouds} %</p>`;
+  document.getElementById(
+    'wind-speed'
+  ).innerHTML = `<p>${allData.dataCurrentWeather.windSpeed} m/s</p>`;
+  document.getElementById(
+    'weather-description'
+  ).innerHTML = `<p>${allData.dataCurrentWeather.weatherDescription}</p>`;
+  document.getElementById(
+    'weather-code'
+  ).innerHTML = `<p>${allData.dataCurrentWeather.weatherCode}</p>`;
+  document.getElementById('trip-image').src = allData.webformatURL;
+}
 
 export { handleSubmit };
