0b7b4c3df9a43a37baa9b4db05950b87c6aef534
Debora Durek <debora.durek@gmail.com>
2021-03-12 19:29:31 -0600

    feat: Import event listener and scss into index.js

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import { handleSubmit } from './js/app';
+import { eventListenerToForm } from './js/listeners';
+
+import './styles/base.scss';
+import './styles/form-section.scss';
+import './styles/results-section.scss';
+import './styles/footer.scss';
+import './styles/page-containers.scss';
+
+document.addEventListener('DOMContentLoaded', eventListenerToForm);
