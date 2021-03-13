d0b5e9b9fa57537bd13982ae5b1efbee9d4ea34c
Debora Durek <debora.durek@gmail.com>
2021-03-12 21:36:56 -0600

    refactor: Create scss variables and import files

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,5 @@
-import { handleSubmit } from './js/app';
 import { eventListenerToForm } from './js/listeners';
 
-import './styles/base.scss';
-import './styles/form-section.scss';
-import './styles/results-section.scss';
-import './styles/footer.scss';
-import './styles/page-containers.scss';
+import './styles/styles.scss';
 
 document.addEventListener('DOMContentLoaded', eventListenerToForm);
